a6b2665f1e27ba780e1302f5d1ee4d7a4343a405
tmknom <tmk.nom@gmail.com>
2016-07-06 13:33:49 +0900

    プロジェクト直下にFabricスクリプト作成

---
+++ fabfile.py
@@ -0,0 +1,7 @@
+# -*- encoding:utf-8 -*-
+
+from configuration.fabfile import itamae_base
+from orchestration.fabfile import build_vpc
+from orchestration.fabfile import delete_vpc
+from orchestration.fabfile import build_security_group
+from orchestration.fabfile import delete_security_group
