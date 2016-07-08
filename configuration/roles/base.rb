856d32c9fb68f36819f93358b0b4f612c911206e
tmknom <tmk.nom@gmail.com>
2016-07-08 14:47:09 +0900

    baseロールにlocaleやタイムゾーンのレシピを組み込み

--- configuration/roles/base.rb
+++ configuration/roles/base.rb
@@ -6,3 +6,8 @@ include_recipe '../cookbooks/sshd/default.rb'
 include_recipe '../cookbooks/rpm_repository/default.rb'
 include_recipe '../cookbooks/rpm_package/default.rb'
 include_recipe '../cookbooks/rpm_package/update.rb'
+
+# Amazon Linux
+include_recipe '../cookbooks/rpm_package/auto_update.rb'
+include_recipe '../cookbooks/timezone/default.rb'
+include_recipe '../cookbooks/locale/default.rb'
