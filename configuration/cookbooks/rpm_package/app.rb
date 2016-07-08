0f5622afecbf536cae2b66575d3220cc7aeffa8f
tmknom <tmk.nom@gmail.com>
2016-07-08 15:25:21 +0900

    mecab-develとipa-gothic-fonts・ipa-mincho-fontsをインストール

--- configuration/cookbooks/rpm_package/app.rb
+++ configuration/cookbooks/rpm_package/app.rb
@@ -2,6 +2,6 @@ package 'http://packages.groonga.org/centos/groonga-release-1.1.0-1.noarch.rpm' 
   not_if 'rpm -q groonga-release'
 end
 
-%w(mecab mecab-ipadic nginx libxml2-devel libxslt-devel ImageMagick-devel patch).each do |pkg|
+%w(mecab mecab-ipadic mecab-devel nginx libxml2-devel libxslt-devel ImageMagick-devel patch ipa-gothic-fonts ipa-mincho-fonts).each do |pkg|
   package pkg
 end
