16fa12407b02e5c2825d1b91cfc31c09cbe6d73b
tmknom <tmk.nom@gmail.com>
2016-07-08 14:55:24 +0900

    MySQLのrpm名にcommunityを付与（毎回実行されてウザいので正式名にした）

--- configuration/cookbooks/mysql_server/default.rb
+++ configuration/cookbooks/mysql_server/default.rb
@@ -2,7 +2,7 @@ package 'http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm' do
   not_if 'rpm -q mysql-community-release'
 end
 
-%w(mysql mysql-devel mysql-server mysql-utilities).each do |pkg|
+%w(mysql-community-client mysql-community-devel mysql-community-server mysql-utilities).each do |pkg|
   package pkg
 end
 
