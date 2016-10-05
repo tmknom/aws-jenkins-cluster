57cc2fe657ba5870271f61eea8c7f8dcdd545f66
tmknom <tmk.nom@gmail.com>
2016-10-05 17:19:31 +0900

    rpmforgeリポジトリのインストールを除外

--- configuration/cookbooks/rpm_repository/default.rb
+++ configuration/cookbooks/rpm_repository/default.rb
@@ -11,14 +11,3 @@ execute 'disable remi' do
   only_if "cat /etc/yum.repos.d/remi.repo | grep enabled | grep -v 0"
   command 'sed -i \'s/enabled\s=\s1/enabled=0/g\' /etc/yum.repos.d/remi.repo'
 end
-
-
-# rpmforge
-package 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm' do
-  not_if 'rpm -q rpmforge-release'
-end
-
-execute 'disable rpmforge' do
-  only_if "cat /etc/yum.repos.d/rpmforge.repo | grep enabled | grep -v 0"
-  command 'sed -i \'s/enabled\s=\s1/enabled=0/g\' /etc/yum.repos.d/rpmforge.repo'
-end
